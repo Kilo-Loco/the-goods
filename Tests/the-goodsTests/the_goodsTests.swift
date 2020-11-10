4378a880917947498a2b5850ba5d11c41bcf5577
Kilo Loco <kyle@kiloloco.com>
2020-11-10 00:09:36 -0800

    Update tests

--- Tests/the-goodsTests/the_goodsTests.swift
+++ Tests/the-goodsTests/the_goodsTests.swift
@@ -1,12 +1,13 @@
 import XCTest
-@testable import the_goods
+@testable import TheGoods
 
 final class the_goodsTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(the_goods().text, "Hello, World!")
+        CoreService.doSomething()
+        XCTAssertTrue(true)
     }
 
     static var allTests = [
